f02412a731797f00e0588933d2cd8c1ec0b09593
xX-Higgs-Xx <20223tn131@utez.edu.mx>
2025-02-19 15:54:45 -0600

    Update routing in App component: change home route to /registro

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ function App() {
     <Router>
       <div className="App">
         <nav className="p-4 bg-gray-800 text-white flex justify-center space-x-4">
-          <Link to="/" className="hover:underline">Inicio</Link>
+          <Link to="/registro" className="hover:underline">Inicio</Link>
           <Link to="/empresas" className="hover:underline">Ver Empresas</Link>
         </nav>
         <Routes>
-          <Route path="/" element={<FormularioRegistro />} />
+          <Route path="/registro" element={<FormularioRegistro />} />
           <Route path="/2025_SDA_I20223TN131_Validar_Entrada" element={<FormularioRegistro />} />
           <Route path="/empresas" element={<EmpresasListado />} />
           <Route path="/empresa/:id" element={<EmpresaDetalle />} />
